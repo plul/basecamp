1c5a378393430102930fbb81cf622e300c6154f2
Asger Juul Brunshøj <asgerbrunshoj@gmail.com>
2024-10-18 13:34:47 +0200

    feat: use nixos-unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
